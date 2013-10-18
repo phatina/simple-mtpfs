0e6e8877884363d2afc8c343bc02aa6220b9b1bd
Peter Hatina <phatina@redhat.com>
2013-10-18 14:49:02 +0200

    switch check order in SMTPFileSystem::exec()

--- src/simple-mtpfs-fuse.cpp
+++ src/simple-mtpfs-fuse.cpp
@@ -380,11 +380,11 @@ bool SMTPFileSystem::exec()
     if (!m_options.m_good)
         return false;
 
-    if (!smtpfs_check_dir(m_options.m_mount_point))
-        return false;
-
     if (m_options.m_version || m_options.m_help)
         return true;
+
+    if (!smtpfs_check_dir(m_options.m_mount_point))
+        return false;
 
     if (!createTmpDir())
         return false;
